00fc2bc8e5f344091389fc0a14da9fb9cf99bb3d
pullopen <66767303+pullopen@users.noreply.github.com>
2020-08-04 13:13:26 +0800

    Update feed_decoder.py

--- utils/feed_decoder.py
+++ utils/feed_decoder.py
@@ -45,7 +45,7 @@ def TweetDecoder(rss_data):
 
   # print(soup.prettify())
   # print(str(data))
-  data['plain'] = unescape(soup.prettify()) + '\n' + config['MASTODON']['Appendix'] + '\n' +config['MASTODON']['BiliSourcePrefix']+' ' + rss_data['link']
+  data['plain'] = unescape(soup.prettify()) + '\n' + config['MASTODON']['Appendix'] + '\n\n' +config['MASTODON']['BiliSourcePrefix']+' ' + rss_data['link']
   return data 
 
 if __name__ == '__main__':
