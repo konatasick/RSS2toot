c8aa2a5236bae44e460e7e2e7429fb67024811d1
pullopen <66767303+pullopen@users.noreply.github.com>
2020-08-14 14:33:40 +0800

    Update feed_decoder.py

--- utils/feed_decoder.py
+++ utils/feed_decoder.py
@@ -35,6 +35,8 @@ def TweetDecoder(rss_data):
         if ('微博视频' in link.getText()):
           shortlink = link.get('data-url')
           link.replace_with(f'''[?bs4_replace_flag?] {config['MASTODON']['VideoSourcePrefix']} {link.getText()} {geturl(shortlink)}[?bs4_replace_flag?]''')
+        elif ('查看图片' in link.getText()):
+          link.replace_with(f'''[?bs4_replace_flag?] {config['MASTODON']['ExternalLinkPrefix']} {link.get('href')}[?bs4_replace_flag?]''')
         else:
           shortlink = link.get('data-url')
           link.replace_with(f'''[?bs4_replace_flag?] {config['MASTODON']['ExternalLinkPrefix']} {geturl(shortlink)}[?bs4_replace_flag?]''')
