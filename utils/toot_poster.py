3865e2217ea329912a1db6ddad1ed63f5de52b78
pullopen <66767303+pullopen@users.noreply.github.com>
2020-11-04 20:20:46 +0800

    Update toot_poster.py

--- utils/toot_poster.py
+++ utils/toot_poster.py
@@ -21,7 +21,6 @@ def TweetDecoder(rss_data):
   :params object: Summary from FeedParaser
   :return object
   """
-  title = str(rss_data['title'])
 
 def TootPoster(data):
   """
@@ -38,7 +37,7 @@ def TootPoster(data):
       media_ids_arr.append(media_post('temp/img%d.png' % id))
 
   try:
-    mastodon.status_post(status=data['plain'], spoiler_text=title, media_ids=media_ids_arr, visibility=config['MASTODON']['TootVisibility'])
+    mastodon.status_post(status=data['plain'], spoiler_text=str(rss_data['title']), media_ids=media_ids_arr, visibility=config['MASTODON']['TootVisibility'])
   except Exception:
     print(f'ERRO: failed[mastodon.status_post]')
     # for e in Exception:
